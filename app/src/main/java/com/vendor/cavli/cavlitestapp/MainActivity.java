0336e173c2b077cd3c35717eac5de7be89928eae
khoa.dang <khoa.dang@cavliwireless.com>
2023-12-06 02:00:46 +0000

    Update demo

--- app/src/main/java/com/vendor/cavli/cavlitestapp/MainActivity.java
+++ app/src/main/java/com/vendor/cavli/cavlitestapp/MainActivity.java
@@ -23,6 +23,8 @@ import vendor.cavli.automotive.vehicle.V1_0.VehicleProperty;
 
 public class MainActivity extends AppCompatActivity {
 
+    final String TAG = "cavlitestapp";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         CavliVehicle mVehicle = new CavliVehicle(looper, vehicleCallback);
 
         try {
+            Log.i(TAG, "subscribe");
             mVehicle.subscribe(opts);
         } catch (RemoteException e) {
 
@@ -49,16 +52,23 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     private class VehicleCallback implements ICavliVehicleCallback {
         @Override
         public void onPropertyEvent(ArrayList<VehiclePropValue> propValues) {
+            Log.d(TAG, "onPropertyEvent propValues.size()=  " + propValues.size());
             for (VehiclePropValue vpv : propValues) {
+                Log.d(TAG, "vpv.prop " + vpv.prop);
                 if (vpv.prop == VehicleProperty.PERF_ODOMETER && vpv.value != null
                         && vpv.value.floatValues != null
                         && vpv.value.floatValues.size() == 1) {
                     float odometer_value = vpv.value.floatValues.get(0);
                     Log.d(TAG, "odometer_value " + odometer_value);
+                }
+                if (vpv.prop == VehicleProperty.PERF_VEHICLE_SPEED && vpv.value != null
+                        && vpv.value.floatValues != null
+                        && vpv.value.floatValues.size() == 1) {
+                    float speed_value = vpv.value.floatValues.get(0);
+                    Log.d(TAG, "speed_value " + speed_value);
                 }
             }
         }
@@ -71,6 +81,11 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onPropertySetError(int errorCode, int propId, int areaId) {
 
+        }
+
+        @Override
+        public void onConnected() {
+            Log.i(TAG, "connected");
         }
     }
 }
